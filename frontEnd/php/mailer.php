2fd5c2864c5dc4b1fb7d65f488cf58199d4a0657
sikB <sikhovski@icloud.com>
2016-06-16 17:26:51 -0400

    styling + adding pages

---
+++ frontEnd/php/mailer.php
@@ -0,0 +1,19 @@
+<?php
+
+
+      require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
+      $mail = new PHPMailer(); // defaults to using php "mail()"
+      $mail->AddReplyTo($_POST['fullName']);
+      $mail->SetFrom($_POST['email']);
+      $mail->AddAddress("sikhovski@icloud.com");
+      $mail->Subject    = "Work: " . 'Message from - '. $_POST['fullName'];
+      $mail->MsgHTML($_POST['comment']);
+
+      if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+      } else {
+      echo "Message sent!";
+      }
+      header('location: http://localhost:8000/index.html#/');
+
+?>
